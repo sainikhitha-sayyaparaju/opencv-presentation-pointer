959e1f203ed80eedf4db5fcf746664dfd37a1470
Sai Nikhitha Sayyaparaju <sainikhitha06112002@gmail.com>
2022-06-26 20:36:52 +0530

    added gestures 3, 4 i.e drawing and eraising

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ gestureThreshold = 300
 buttonPressed = False
 buttonCounter = 0
 buttonDelay = 20
+annotations = []
+annotationStart = False
+annotationNumber = -1
 
 #camera setup
 cap =   cv2.VideoCapture(0)
@@ -50,33 +53,59 @@ while True:
         indexFinger = [xVal, yVal]
         fingers = detector.fingersUp(hand)
         if cy <= gestureThreshold: #if hand is at the height of the threshold then we will consider the gestures
+            annotationStart = False
             #gesture 1 - move left
             if fingers == [1,0,0,0,0]:
-
+                annotationStart = False
                 print("left")
                 if imgNumber > 0:
+                    annotations = []
+                    annotationNumber = -1
                     imgNumber -= 1
                     buttonPressed = True
             # gesture 2 - move right
             if fingers == [0, 0, 0, 0, 1]:
-
+                annotationStart = False
                 print("right")
                 if imgNumber < len(pathimages) - 1:
+                    annotations = []
+                    annotationNumber = -1
                     imgNumber += 1
                     buttonPressed = True
         # gesture 3 - show pointer
         if fingers == [0, 1, 1, 0, 0]:
             cv2.circle(imgCurrent, indexFinger, 12, (0,0,255), cv2.FILLED)
+            annotationStart = False
         # gesture 4 - draw
         if fingers == [0, 1, 0, 0, 0]:
+            if annotationStart == False:
+                annotationStart = True
+                annotationNumber += 1
+                annotations.append([])
             cv2.circle(imgCurrent, indexFinger, 12, (0, 0, 255), cv2.FILLED)
+            annotations[annotationNumber].append(indexFinger)
+        else:
+            annotationStart = False
+
+        # gesture - 5 - eraise
+        if fingers == [0, 1, 1, 1, 0]:
+            if annotations:
+                if annotationNumber >= 0:
+                    annotations.pop(-1)
+                    annotationNumber -= 1
+                    buttonPressed = True
+    else:
+        annotationStart = False
     if buttonPressed:
         buttonCounter += 1
         if buttonCounter >= buttonDelay:
             buttonCounter = 0
             buttonPressed = False
 
-
+    for i in range(len(annotations)):
+        for j in range(len(annotations[i])):
+            if j != 0:
+                cv2.line(imgCurrent, annotations[i][j - 1], annotations[i][j], (0, 0, 200), 12)
 
     imgCurrent[h - hs: h, w - ws:w] = imgSmall
     cv2.imshow("Image", imgSmall)
